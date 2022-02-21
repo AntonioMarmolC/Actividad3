73dadd226e6524523dbd04421a4986e8d2243595
Antonio <antoniomarmol@paucasesnovescifp.cat>
2022-02-21 20:13:23 +0100

    Commit actividad 3

---
+++ Actvidad3/src/Act3.java
@@ -0,0 +1,18 @@
+
+public class Act3
+{
+     public static void main(String[] args)
+    {
+        for (int i = 7; i <100; i++)
+        {
+            if (i % 7 == 0)
+                {
+                    System.out.println(i);
+                }
+        } 
+        for (int i = 7; i < 100; i += 7)
+        {
+            System.out.println(i);
+        } 
+    }
+}
